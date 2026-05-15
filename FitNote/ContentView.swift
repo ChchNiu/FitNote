6f44eb63af22f60b89869817e4f096c423d28714
ncc <2451982038@qq.com>
2026-05-15 15:18:45 +0800

    Initial Commit

---
+++ FitNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitNote
+//
+//  Created by 牛超超 on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
